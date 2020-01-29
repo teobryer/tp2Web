8e7beebb8d60aaed535118481f4170eb62674016
teobryer <teobryer@h001-3>
2020-01-29 14:21:11 +0100

    employee class

---
+++ src/Employee.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Employee {
+    
+    private $id;
+    private $name;
+    private $salary;
+    private $age;
+    
+    function __construct(){
+        
+    }
+    
+    function __destruct(){
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
