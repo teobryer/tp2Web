0ad40986f06f9cbd948666758e0427f59b55574c
teobryer <teobryer@h001-3>
2020-01-29 14:33:37 +0100

    construct, to string getters & setters

--- src/Employee.php
+++ src/Employee.php
@@ -1,18 +1,92 @@
 <?php 
 
-class Employee {
+class Employee implements IEmployee {
     
     private $id;
     private $name;
     private $salary;
     private $age;
     
-    function __construct(){
-        
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $salary
+     */
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    function __construct(int $id, string $name, float $salary, int $age){
+        $this->id = $id;
+        $this->name = $name;
+        $this->salary = $salary;
+        $this->age = $age;
     }
     
     function __destruct(){
         
+    }
+    
+    function __toString(){
+        $t=get_object_vars($this);
+        $s="";
+        foreach($t as $key=>$value)$s.="$key = $value\n";
+        return $s;
     }
     
 }
